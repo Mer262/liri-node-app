68b6f8f73a844b3759275cb7eb344cf6dd164532
Meredith Moore <meredithmoore@Merediths-MacBook-Pro.local>
2017-02-15 21:41:24 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'ctM35nOy1zpx89T5mSsykHNp6',
+    consumer_secret: 'TMLHCom8BPoRhwMrIbmsOVbkmOU6aj5YCE496FevqEPzPGlGQ7',
+    access_token_key: '1061824920-NBZCsFsA7gQ8m9L5froZi2QTZx3tpVwTDQIsXvm',
+    access_token_secret: 'RMB9Y9Avwz3KhwQkl83C9Giy1s91Q6WLSVk5eEC6urTKy',
+}
